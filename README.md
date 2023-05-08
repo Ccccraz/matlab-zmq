d7baebf48f24de70003691062be2b66897d7a224
iandol <iandol@cog5.org>
2023-05-08 17:38:11 +0800

    Add precompiled mex for Linux

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 matlab-zmq
 ==========
 
+**NOTE:** this fork is slightly modified to include precompiled mex files for Linux, and include some minimal pull requests from other forks.
+
 What's this all about?
 ----------------------
 
@@ -26,8 +28,9 @@ Stuff Doesn't Work
   - Versions: MATLAB, ZMQ and Operating System
   - Which version of matlab-zmq you're using (i.e. which branch, commit etc).
   - What you are trying to do - preferably include a succinct code example which illustrates the problem.
-  - What doesn't work - please describe behaviour explicitly and include any error messages you encounter.
-
+  - What doesn't work - please describe behavior explicitly and include any error messages you encounter.
+  - When building with Matlab R2022b, you may need to launch Matlab with the environment variable `LD_PRELOAD` set to the path to your system's `libstdc++` binary
+  - e.g. `LD_PRELOAD=/lib/x86_64-linux-gnu/libstdc++.so.6 matlab` on Linux systems
 How can I help?
 -----------
 
--- config.m
+++ config.m
@@ -1,14 +1,17 @@
 % Please edit this file with the correct paths for ZMQ instalation.
-%
-% Examples can be found in files `config_unix.m`, `config_win.m`.
-% This file itself shows how to build `matlab-zmq` using a Homebrew
-% installation of ZMQ 4.0.4 for OS-X.
 
-% ZMQ library filename
-ZMQ_COMPILED_LIB = './libzmq.a';
-
-% ZMQ library path
-ZMQ_LIB_PATH = '/usr/local/Cellar/zeromq/4.0.4/lib/';
-
-% ZMQ headers path
-ZMQ_INCLUDE_PATH = '/usr/local/Cellar/zeromq/4.0.4/include/';
+if (ismac)
+	ZMQ_COMPILED_LIB = './libzmq.a';
+	ZMQ_LIB_PATH = '/usr/local/Cellar/zeromq/4.3.4/lib/';
+	ZMQ_INCLUDE_PATH = '/usr/local/Cellar/zeromq/4.3.4/include/';
+elseif (isunix)
+	ZMQ_COMPILED_LIB = 'libzmq.a';
+	ZMQ_LIB_PATH = '/usr/lib/x86_64-linux-gnu/';
+	ZMQ_INCLUDE_PATH = '/usr/include/';
+elseif (ispc)
+	ZMQ_COMPILED_LIB = 'libzmq-v120-mt-4_0_4.lib';
+	ZMQ_LIB_PATH = 'C:\Program Files\ZeroMQ 4.0.4\lib\';
+	ZMQ_INCLUDE_PATH = 'C:\Program Files\ZeroMQ 4.0.4\include';
+else
+	error("libzmq install paths need to be added to config.m for your platform")
+end
